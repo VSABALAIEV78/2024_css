2216490c668d8182292eb503edce0dc2a647b14e
Vadym Sabalaiev <103932394+VSABALAIEV78@users.noreply.github.com>
2024-02-19 22:31:10 +0200

    50% dovne

---
+++ script.js
@@ -0,0 +1,10 @@
+// window.addEventListener('scroll', function() {
+//     var scrollDiv = document.getElementById('scrollDiv');
+//     var scrollPosition = window.scrollY;
+
+//     if (scrollPosition > 500) {
+//       scrollDiv.style.backgroundColor = 'transparent';
+//     } else {
+//       scrollDiv.style.backgroundColor = 'rgb(52, 0, 95)'; // Revert back to original color
+//     }
+//   });
